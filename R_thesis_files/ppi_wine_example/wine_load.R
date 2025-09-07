04ecac38e873e5f3c69713959c366a0ebc3a1bab
rogerli133 <97822177+rogerli133@users.noreply.github.com>
2025-09-07 19:23:36 +0200

    Final Thesis Update

--- R_thesis_files/ppi_wine_example/wine_load.R
+++ R_thesis_files/ppi_wine_example/wine_load.R
@@ -3,15 +3,21 @@ set.seed(1)
 wine <- read_csv("data/wine_quality_labelled.csv")
 
 # separate data into expert and novice dfs
+
+# 100 x 12 df of wines tasted by both experts and novices
+# responses here are from the expert
 wine_exp <- wine %>% 
   filter(label == "expert") %>% 
   select(-label)
 
+# 100 x 12 df of wines tasted by both experts and novices
+# responses here are from the novices
+wine_compare <- wine %>% 
+  filter(label == "compare") %>% 
+  select(-label)
+
+# 3000 x 12 df of wines tasted only by novies
 wine_nov <- wine %>% 
   filter(label == "novice") %>% 
   select(-label)
 
-# separate the wines tasted by both expert and novices (Y1, f(X1)), ..., (Yn, f(Xn))
-wine_compare <- wine %>% 
-  filter(label == "compare") %>% 
-  select(-label)
